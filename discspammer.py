3e2b40be221abd491d34d69d452d1596077e7e8c
Psyrachi <60986729+Psyrachi@users.noreply.github.com>
2020-06-26 16:19:46 -0400

    Update discspammer.py

--- discspammer.py
+++ discspammer.py
@@ -3,6 +3,6 @@ import pyautogui
 pyautogui.hotkey('alt', 'tab')
 pyautogui.leftClick(580,950,1,.3)
 
-x = 1
-while x == 1:
+inDiscord = True
+while inDiscord True:
     pyautogui.typewrite(['a', 'enter'])
